5e4306058f2b092044c06f50531928b7789fc6b3
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-01-15 23:57:08 +0530

    added Post, Tag, Profile Entity

---
+++ backend/blog/src/main/java/com/jk/blog/entity/Post.java
@@ -0,0 +1,59 @@
+package com.jk.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.HashSet;
+import java.util.Set;
+
+@Entity
+@Table(name = "posts")
+@NoArgsConstructor
+@Getter
+@Setter
+public class Post {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long postId;
+    @Column(length = 100, nullable = false)
+    private String postTitle;
+    @Lob
+    private String postContent;
+    private String imageName;
+    private String videoName;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date createdDate;
+    @Temporal(TemporalType.TIMESTAMP)
+    private Date lastUpdatedDate;
+    @Column(nullable = false)
+    private boolean isLive = true; // Default value set to true
+
+    @ManyToOne
+    @JoinColumn(name = "category_id", nullable = false)
+    private Category category;
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToMany
+    @JoinTable(
+            name = "post_tag",
+            joinColumns = @JoinColumn(name = "post_id"),
+            inverseJoinColumns = @JoinColumn(name = "tag_id")
+    )
+    private Set<Tag> tags = new HashSet<>();
+
+    @PrePersist
+    protected void onCreate() {
+        createdDate = new Date();
+    }
+
+    @PreUpdate
+    protected void onUpdate() {
+        lastUpdatedDate = new Date();
+    }
+}
--- backend/blog/src/main/java/com/jk/blog/entity/User.java
+++ backend/blog/src/main/java/com/jk/blog/entity/User.java
@@ -1,15 +1,12 @@
 package com.jk.blog.entity;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
-import lombok.Data;
+import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Entity
 @Table(name = "users")
@@ -20,15 +17,20 @@ public class User {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int id;
-
-    @Column(name = "name", nullable = false, length = 50)
+    private Long userId;
+    @Column(name = "user_name")
+    private String userName;
+    @Column(nullable = false, length = 50)
     private String name;
-    @Column(name = "email", nullable = false)
+    @Column(nullable = false)
     private String email;
-    @Column(name = "password", nullable = false)
+    @Column(nullable = false)
     private String password;
-    @Column(name = "active")
     private boolean active;
-    
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private List<Post> posts = new ArrayList<>();
+
+    @OneToOne(mappedBy = "user", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    private Profile profile;
 }
