ff348819bdde450306a061c2bf68d048eb5b0207
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-01-21 05:32:11 +0530

    added Comment Entity

---
+++ backend/blog/src/main/java/com/jk/blog/entity/Comment.java
@@ -0,0 +1,27 @@
+package com.jk.blog.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "comments")
+@Getter
+@Setter
+@NoArgsConstructor
+public class Comment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long commentId;
+    private String commentDesc;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @ManyToOne
+    @JoinColumn(name = "post_id", nullable = false)
+    private Post post;
+}
