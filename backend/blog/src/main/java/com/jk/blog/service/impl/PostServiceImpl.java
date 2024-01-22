9efb0402cd9a9edd521c43efadc225d06701ce04
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-01-22 20:46:26 +0530

    Add Pagination and Sorting to getAll method in PostService

--- backend/blog/src/main/java/com/jk/blog/service/impl/PostServiceImpl.java
+++ backend/blog/src/main/java/com/jk/blog/service/impl/PostServiceImpl.java
@@ -1,6 +1,8 @@
 package com.jk.blog.service.impl;
 
-import com.jk.blog.dto.PostDTO;
+import com.jk.blog.dto.PageableResponse;
+import com.jk.blog.dto.PostRequestBody;
+import com.jk.blog.dto.PostResponseBody;
 import com.jk.blog.entity.Category;
 import com.jk.blog.entity.Post;
 import com.jk.blog.entity.User;
@@ -11,9 +13,12 @@ import com.jk.blog.repository.UserRepository;
 import com.jk.blog.service.PostService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
 
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -30,13 +35,13 @@ public class PostServiceImpl implements PostService {
     private ModelMapper modelMapper;
 
     @Override
-    public PostDTO createPost(PostDTO postDTO) {
+    public PostResponseBody createPost(PostRequestBody postRequestBody) {
 
         User user = userRepository
-                    .findById(postDTO.getUserId())
-                    .orElseThrow(() -> new ResourceNotFoundException("User", "userId", postDTO.getUserId()));
-        Category category = categoryRepository.findById(postDTO.getCategoryId()).orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", postDTO.getCategoryId()));
-        Post post = this.modelMapper.map(postDTO, Post.class);
+                    .findById(postRequestBody.getUserId())
+                    .orElseThrow(() -> new ResourceNotFoundException("User", "userId", postRequestBody.getUserId()));
+        Category category = categoryRepository.findById(postRequestBody.getCategoryId()).orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", postRequestBody.getCategoryId()));
+        Post post = this.modelMapper.map(postRequestBody, Post.class);
 
         post.setUser(user);
         post.setCategory(category);
@@ -46,46 +51,59 @@ public class PostServiceImpl implements PostService {
         post.setLastUpdatedDate(new Date());
 */
         Post savedPost = this.postRepository.save(post);
-        return this.modelMapper.map(savedPost, PostDTO.class);
+        return this.modelMapper.map(savedPost, PostResponseBody.class);
     }
 
     @Override
-    public PostDTO getPostById(Long postId) {
+    public PostResponseBody getPostById(Long postId) {
         Post post = this.postRepository
                         .findById(postId)
                         .orElseThrow(() -> new ResourceNotFoundException("Post", "postId", postId));
-        return this.modelMapper.map(post, PostDTO.class);
+        return this.modelMapper.map(post, PostResponseBody.class);
 
     }
 
     @Override
-    public List<PostDTO> getAllPost() {
-        List<Post> postList = this.postRepository.findAll();
-        return postList.stream()
-                       .map((eachPost) -> this.modelMapper.map(eachPost, PostDTO.class))
-                       .collect(Collectors.toList());
+    public PageableResponse<PostResponseBody> getAllPost(Integer pageNumber, Integer pageSize, String sortBy, String sortDirection) {
+        Sort sort = sortDirection.equalsIgnoreCase("asc") ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
+        Page<Post> pagePost = this.postRepository.findAll(pageable);
+        List<Post> postList = pagePost.getContent();
+
+        List<PostResponseBody> postResponseBodyList = postList.stream()
+                                                       .map((eachPost) -> this.modelMapper.map(eachPost, PostResponseBody.class))
+                                                       .collect(Collectors.toList());
+
+        PageableResponse<PostResponseBody> pageableResponse = new PageableResponse<>();
+        pageableResponse.setContent(postResponseBodyList);
+        pageableResponse.setPageNumber(pagePost.getNumber());
+        pageableResponse.setSize(pagePost.getSize());
+        pageableResponse.setTotalElements(pagePost.getTotalElements());
+        pageableResponse.setTotalPages(pagePost.getTotalPages());
+        pageableResponse.setLastPage(pagePost.isLast());
+        return pageableResponse;
     }
 
     @Override
-    public PostDTO updatePost(PostDTO postDTO, Long postId) {
+    public PostResponseBody updatePost(PostRequestBody postRequestBody, Long postId) {
         Post existingPost = this.postRepository
                         .findById(postId)
                         .orElseThrow(() -> new ResourceNotFoundException("Post", "postId", postId));
     /*
          post.setPostTitle(postDTO.getTitle());
          post.setPostContent(postDTO.getContent());
-         post.setImageName(postDTO.getImageName());
-         post.setVideoName(postDTO.getVideoName());
+         post.setImageUrl(postDTO.getImageUrl());
+         post.setVideoUrl(postDTO.getVideoUrl());
          post.setLive(postDTO.getIsLive());
     */
         // above is not needed if used below 82 to 85
-        this.modelMapper.typeMap(PostDTO.class, Post.class).addMappings(mapper -> {
+        this.modelMapper.typeMap(PostRequestBody.class, Post.class).addMappings(mapper -> {
             mapper.skip(Post::setUser);
         });
-        this.modelMapper.map(postDTO, existingPost);
+        this.modelMapper.map(postRequestBody, existingPost);
 
         Post updatedPost = this.postRepository.save(existingPost);
-        return this.modelMapper.map(updatedPost, PostDTO.class);
+        return this.modelMapper.map(updatedPost, PostResponseBody.class);
     }
 
     @Override
@@ -97,32 +115,32 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
-    public List<PostDTO> getPostsByUser(Long userId) {
+    public List<PostResponseBody> getPostsByUser(Long userId) {
         User existingUser = this.userRepository
                 .findById(userId)
                 .orElseThrow(() -> new ResourceNotFoundException("User", "userId", userId));
         List<Post> existingPostList = this.postRepository.findByUser(existingUser);
         return existingPostList.stream()
-                .map((eachPost) -> this.modelMapper.map(eachPost, PostDTO.class))
+                .map((eachPost) -> this.modelMapper.map(eachPost, PostResponseBody.class))
                 .collect(Collectors.toList());
     }
 
     @Override
-    public List<PostDTO> getPostsByCategory(Long categoryId) {
+    public List<PostResponseBody> getPostsByCategory(Long categoryId) {
         Category existingCategory = this.categoryRepository
                                         .findById(categoryId)
                                         .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", categoryId));
         List<Post> existingPostList = this.postRepository.findByCategory(existingCategory);
         return existingPostList.stream()
-                               .map((eachPost) -> this.modelMapper.map(eachPost, PostDTO.class))
+                               .map((eachPost) -> this.modelMapper.map(eachPost, PostResponseBody.class))
                                .collect(Collectors.toList());
     }
 
     @Override
-    public List<PostDTO> searchPostsByTitle(String keyword) {
+    public List<PostResponseBody> searchPostsByTitle(String keyword) {
         List<Post> existingPostList = this.postRepository.findByTitleContainingIgnoreCase(keyword);
         return existingPostList.stream()
-                .map((eachPost) -> this.modelMapper.map(eachPost, PostDTO.class))
+                .map((eachPost) -> this.modelMapper.map(eachPost, PostResponseBody.class))
                 .collect(Collectors.toList());
     }
 }
