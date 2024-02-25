6f9840cc00652ff6945bffd19d0d63fc3fba4098
Jitendra Tiwari <48156628+jkjitendra@users.noreply.github.com>
2024-02-25 23:53:39 +0530

    Feature/initial setup (#11)
    * Implemented Category CRUD and added request validation for Category and User with global exception handling
    
    * added Post, Tag, Profile Entity
    
    * Refactor User entity: Convert primitives to wrappers, add comments field
    
    * Refactor Category entity: Convert primitives to wrappers
    
    * Refactor: Convert primitives to wrappers
    
    * Added Post entity, service, repo, and CRUD in controller
    
    * added Comment Entity
    
    * Refactor User and Post DTOs and Enhance Validation
    
    * Add Pagination and Sorting to getAll method in PostService
    
    * updated springboot version, added google libphonenumber for validation, updated User crud operations, added checkusernameavailability feature, added mobile and countryName to USER

--- backend/blog/src/main/java/com/jk/blog/dto/UserRequestBody.java
+++ backend/blog/src/main/java/com/jk/blog/dto/UserRequestBody.java
@@ -14,15 +14,25 @@ import lombok.Setter;
 public class UserRequestBody {
     private Long id;
     @NotEmpty
-    @Size(min = 4, message = "name should be greater than 4")
+    @Size(min = 4, message = "Name Should Be Greater Than 4")
     private String name;
+
     @NotEmpty
-    @Size(min = 4, message = "username should be greater than 4")
+    @Size(min = 4, message = "Username Should Be Greater Than 4")
     private String userName;
-    @Email(message = "Please enter valid email")
+
+    @Email(message = "Please Enter Valid Email")
     private String email;
-    @NotEmpty
-    @Size(min = 6, message = "Password must be greater than 6")
-    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[@$!%*?&])[A-Za-z\\d@$!%*?&]{8,}$", message = "Password should be combination of numbers, lowercase, uppercase, special Characters")
+
+    @NotEmpty(message = "Password is Required")
+    @Pattern(regexp = "^(?=.*[a-z])(?=.*[A-Z])(?=.*\\d)(?=.*[\\W_])[A-Za-z\\d\\W_]{8,}$",
+            message = "Password Must Be at least 8 Characters long and include at least one UPPERCASE letter, one lowercase letter, one Number, and one Special Character")
     private String password;
+
+    @NotEmpty(message = "Mobile number must not be empty")
+    @Pattern(regexp = "^\\+?[1-9][0-9]{7,14}$", message = "Invalid Mobile Number Format")
+    private String mobile;
+
+    @NotEmpty(message = "Country Name Must Not Be Empty")
+    private String countryName;
 }
--- backend/blog/src/main/java/com/jk/blog/service/UserService.java
+++ backend/blog/src/main/java/com/jk/blog/service/UserService.java
@@ -2,6 +2,7 @@ package com.jk.blog.service;
 
 import com.jk.blog.dto.UserRequestBody;
 import com.jk.blog.dto.UserResponseBody;
+import org.springframework.web.bind.annotation.RequestBody;
 
 import java.util.List;
 
@@ -12,4 +13,8 @@ public interface UserService {
     UserResponseBody getUserById(Long userId);
     List<UserResponseBody> getAllUsers();
     void deleteUser(Long userId);
+    boolean checkUsernameAvailability(String username);
+//    void initiatePasswordReset(String email);
+//    void verifyAndResetPassword(String token, String newPassword, String email);
+
 }
