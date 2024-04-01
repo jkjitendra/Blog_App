c0282fcb9418adee66e33eec33c8344d2f248352
Jitendra Tiwari <48156628+jkjitendra@users.noreply.github.com>
2024-04-01 23:21:48 +0530

    Feature/initial setup (#23)
    * Implemented Category CRUD and added request validation for Category and User with global exception handling
    
    * added Post, Tag, Profile Entity
    
    * Refactor User entity: Convert primitives to wrappers, add comments field
    
    * Refactor Category entity: Convert primitives to wrappers
    
    * Refactor: Convert primitives to wrappers
    
    * Added Post entity, service, repo, and CRUD in controller
    
    * added Comment Entity
    
    * Refactor User and Post DTOs and Enhance Validation
    
    * Add Pagination and Sorting to getAll method in PostService
    
    * updated springboot version, added google libphonenumber for validation, updated User crud operations, added checkusernameavailability feature, added mobile and countryName to USER
    
    * added @Transactional annotation,
    added ExceptionHandler for IllegalArgumentException,
    Updated ResourceNotFoundException to accept String as well as Long values
    
    * added constants,
    added configuration for file upload,
    implemented fileService for file upload,
    added CountryToRegionCodeUtil and PhoneNumberValidationUtil class
    
    * updated hibernate dialect config,
    added show-sql
    
    * added Profile CRUD operations
    
    * updated mobile number format to store as (+,countryCode, mobile)
    
    * separated phonenumber format creation.
    
    * updated Profile entity with @JsonStringType
    
    * updated requestbody name from updates to profile for patchProfile
    
    * Add countryCode to updateUser API for mobile number
    
    * Updated UniqueConstraint for Tag Entity, added findByTagName method to TagRepository
    
    * Extended PostResponseBody with postId and tagNames fields.
    Enhanced PostRequestBody with comments and tagNames fields."

--- backend/blog/src/main/java/com/jk/blog/dto/PostResponseBody.java
+++ backend/blog/src/main/java/com/jk/blog/dto/PostResponseBody.java
@@ -4,17 +4,31 @@ import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
 
+import java.util.HashSet;
+import java.util.Set;
+
 
 @Getter
 @Setter
 @NoArgsConstructor
 public class PostResponseBody {
 
+    private Long postId;
+
     private String title;
+
     private String content;
+
     private String imageUrl;
+
     private String videoUrl;
+
     private Long userId;
+
     private Long categoryId;
+
     private Boolean isLive;
+
+    private Set<String> tagNames = new HashSet<>();
+
 }
