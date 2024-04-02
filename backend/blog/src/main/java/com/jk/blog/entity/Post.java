bbdff3d30c196555bb4e8d965a1bde9744d6cddf
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-04-02 23:57:34 +0530

    added Longtext to postContent column, updated name of post_tag name, added new methods

--- backend/blog/src/main/java/com/jk/blog/entity/Post.java
+++ backend/blog/src/main/java/com/jk/blog/entity/Post.java
@@ -1,5 +1,7 @@
 package com.jk.blog.entity;
 
+import com.fasterxml.jackson.annotation.JsonIdentityInfo;
+import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -12,37 +14,49 @@ import java.util.*;
 @NoArgsConstructor
 @Getter
 @Setter
+//@JsonIdentityInfo(
+//        generator = ObjectIdGenerators.PropertyGenerator.class,
+//        property = "postId"
+//)
 public class Post {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long postId;
+
     @Column(length = 100, nullable = false)
     private String postTitle;
+
     @Lob
+    @Column(columnDefinition="LONGTEXT")
     private String postContent;
+
     private String imageUrl;
     private String videoUrl;
+
     @Temporal(TemporalType.TIMESTAMP)
     private Date createdDate;
+
     @Temporal(TemporalType.TIMESTAMP)
     private Date lastUpdatedDate;
+
     @Column(nullable = false)
     private boolean isLive = true; // Default value set to true
 
     @ManyToOne
     @JoinColumn(name = "category_id", nullable = false)
     private Category category;
+
     @ManyToOne
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
 
-    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, fetch = FetchType.LAZY)
+    @OneToMany(mappedBy = "post", cascade = CascadeType.ALL, fetch = FetchType.LAZY, orphanRemoval = true)
     private List<Comment> comments = new ArrayList<>();
 
-    @ManyToMany
+    @ManyToMany(cascade = {CascadeType.PERSIST, CascadeType.MERGE})
     @JoinTable(
-            name = "post_tag",
+            name = "post_tags",
             joinColumns = @JoinColumn(name = "post_id"),
             inverseJoinColumns = @JoinColumn(name = "tag_id")
     )
@@ -56,5 +70,27 @@ public class Post {
     @PreUpdate
     protected void onUpdate() {
         lastUpdatedDate = new Date();
+    }
+
+    // Utility methods for adding/removing comments
+    public void addComment(Comment comment) {
+        this.comments.add(comment);
+        comment.setPost(this);
+    }
+
+    public void removeComment(Comment comment) {
+        this.comments.remove(comment);
+        comment.setPost(null);
+    }
+
+    // Utility methods for adding/removing tags
+    public void addTag(Tag tag) {
+        this.tags.add(tag);
+        tag.getPosts().add(this);
+    }
+
+    public void removeTag(Tag tag) {
+        this.tags.remove(tag);
+        tag.getPosts().remove(this);
     }
 }
