3997f6da0324a2875ff06865403e2f06ed4347bd
Jitendra Tiwari <48156628+jkjitendra@users.noreply.github.com>
2024-04-02 23:56:32 +0530

    Feature/initial setup (#25)
    * Implemented Category CRUD and added request validation for Category and User with global exception handling
    
    * added Post, Tag, Profile Entity
    
    * Refactor User entity: Convert primitives to wrappers, add comments field
    
    * Refactor Category entity: Convert primitives to wrappers
    
    * Refactor: Convert primitives to wrappers
    
    * Added Post entity, service, repo, and CRUD in controller
    
    * added Comment Entity
    
    * Refactor User and Post DTOs and Enhance Validation
    
    * Add Pagination and Sorting to getAll method in PostService
    
    * updated springboot version, added google libphonenumber for validation, updated User crud operations, added checkusernameavailability feature, added mobile and countryName to USER
    
    * added @Transactional annotation,
    added ExceptionHandler for IllegalArgumentException,
    Updated ResourceNotFoundException to accept String as well as Long values
    
    * added constants,
    added configuration for file upload,
    implemented fileService for file upload,
    added CountryToRegionCodeUtil and PhoneNumberValidationUtil class
    
    * updated hibernate dialect config,
    added show-sql
    
    * added Profile CRUD operations
    
    * updated mobile number format to store as (+,countryCode, mobile)
    
    * separated phonenumber format creation.
    
    * updated Profile entity with @JsonStringType
    
    * updated requestbody name from updates to profile for patchProfile
    
    * Add countryCode to updateUser API for mobile number
    
    * Updated UniqueConstraint for Tag Entity, added findByTagName method to TagRepository
    
    * Extended PostResponseBody with postId and tagNames fields.
    Enhanced PostRequestBody with comments and tagNames fields."
    
    * Added searchKeyOnTitle method to the PostRepository.
    
    * PostController CRUD operations

--- backend/blog/src/main/java/com/jk/blog/controller/PostController.java
+++ backend/blog/src/main/java/com/jk/blog/controller/PostController.java
@@ -1,17 +1,32 @@
 package com.jk.blog.controller;
 
-import com.jk.blog.dto.APIResponse;
-import com.jk.blog.dto.PageableResponse;
-import com.jk.blog.dto.PostRequestBody;
-import com.jk.blog.dto.PostResponseBody;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.jk.blog.constants.AppConstants;
+import com.jk.blog.dto.*;
+import com.jk.blog.entity.User;
+import com.jk.blog.exception.ResourceNotFoundException;
+import com.jk.blog.repository.UserRepository;
+import com.jk.blog.service.FileService;
 import com.jk.blog.service.PostService;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
+import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.http.HttpStatus;
+import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
+import org.springframework.util.StreamUtils;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
+import java.io.IOException;
+import java.io.InputStream;
 import java.util.List;
+import java.util.Map;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("/api/v1/posts")
@@ -19,19 +34,48 @@ public class PostController {
 
     @Autowired
     private PostService postService;
+    @Autowired
+    private UserRepository userRepository;
 
-    @PostMapping("/")
-    public ResponseEntity<PostResponseBody> createPost(@Valid @RequestBody PostRequestBody postRequestBody) {
-        PostResponseBody createdPostRequestBody = this.postService.createPost(postRequestBody);
-        return new ResponseEntity<>(createdPostRequestBody, HttpStatus.CREATED);
+    @Autowired
+    @Qualifier("localFileService")
+//    @Qualifier("s3FileService")
+    private FileService fileService;
+
+    @Autowired
+    private ModelMapper modelMapper;
+
+    @Value("${project.files}")
+    private String path;
+
+    @PostMapping(value = "/user/{userId}/posts", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<PostResponseBody> createPost(
+                                                       @PathVariable Long userId,
+                                                       @Valid @RequestPart("post") String postRequestBody,
+                                                       @RequestPart(value = "image", required = false) MultipartFile image,
+                                                       @RequestPart(value = "video", required = false) MultipartFile video) throws IOException {
+        this.userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("Post", "userId", userId));
+        PostRequestBody postJSON = new ObjectMapper().readValue(postRequestBody, PostRequestBody.class);
+        postJSON.setUserId(userId);
+        if (image != null && !image.isEmpty()) {
+            String imageUrl = fileService.uploadImage(path, image);
+            postJSON.setImageUrl(imageUrl);
+        }
+        if (video != null && !video.isEmpty()) {
+            String videoUrl = fileService.uploadVideo(path, video);
+            postJSON.setVideoUrl(videoUrl);
+        }
+        PostResponseBody createdPostResponseBody = this.postService.createPost(userId, postJSON);
+        System.out.println("createdPostResponseBody " + createdPostResponseBody);
+        return new ResponseEntity<>(createdPostResponseBody, HttpStatus.CREATED);
     }
 
     @GetMapping("/")
     public ResponseEntity<PageableResponse<PostResponseBody>> getAllPost(
-                            @RequestParam(value = "pageNumber", defaultValue = "0", required = false) Integer pageNumber,
-                            @RequestParam(value = "pageSize", defaultValue = "10", required = false) Integer pageSize,
-                            @RequestParam(value = "sortBy", defaultValue = "postId", required = false) String sortBy,
-                            @RequestParam(value = "sortDirection", defaultValue = "asc", required = false) String sortDirection
+                            @RequestParam(value = "pageNumber", defaultValue = AppConstants.PAGE_NUMBER, required = false) Integer pageNumber,
+                            @RequestParam(value = "pageSize", defaultValue = AppConstants.PAGE_SIZE, required = false) Integer pageSize,
+                            @RequestParam(value = "sortBy", defaultValue = AppConstants.SORT_BY, required = false) String sortBy,
+                            @RequestParam(value = "sortDirection", defaultValue = AppConstants.SORT_DIR, required = false) String sortDirection
                             ) {
         PageableResponse<PostResponseBody> pageableResponse = this.postService.getAllPost(pageNumber, pageSize, sortBy, sortDirection);
         return new ResponseEntity<>(pageableResponse, HttpStatus.OK);
@@ -43,14 +87,52 @@ public class PostController {
         return new ResponseEntity<>(existingPostRequestBody, HttpStatus.OK);
     }
 
-    @PutMapping("/{postId}")
-    public ResponseEntity<PostResponseBody> updatePost(@Valid @RequestBody PostRequestBody postRequestBody, @PathVariable Long postId) {
-        PostResponseBody updatePost = this.postService.updatePost(postRequestBody, postId);
+    @PutMapping(value = "/user/{userId}/posts/{postId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<PostResponseBody> updatePost(@PathVariable Long userId,
+                                                       @PathVariable Long postId,
+                                                       @Valid @RequestPart("post") String postRequestBody,
+                                                       @RequestPart(value = "image", required = false) MultipartFile image,
+                                                       @RequestPart(value = "video", required = false) MultipartFile video) throws IOException {
+
+        this.userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("Post", "userId", userId));
+        PostRequestBody postJSON = new ObjectMapper().readValue(postRequestBody, PostRequestBody.class);
+
+        if (image != null && !image.isEmpty()) {
+            String imageUrl = fileService.uploadImage(path, image);
+            postJSON.setImageUrl(imageUrl);
+        }
+        if (video != null && !video.isEmpty()) {
+            String videoUrl = fileService.uploadVideo(path, video);
+            postJSON.setVideoUrl(videoUrl);
+        }
+        PostResponseBody updatePost = this.postService.updatePost(postJSON, postId);
         return new ResponseEntity<>(updatePost, HttpStatus.OK);
     }
 
-    @DeleteMapping("/{postId}")
-    public ResponseEntity<APIResponse> deleteUser(@PathVariable Long postId) {
+    @PatchMapping(value = "/user/{userId}/posts/{postId}", consumes = MediaType.MULTIPART_FORM_DATA_VALUE)
+    public ResponseEntity<PostResponseBody> patchPost( @PathVariable Long userId,
+                                                       @PathVariable Long postId,
+                                                       @RequestPart("post") String updatesJson,
+                                                       @RequestPart(value = "image", required = false) MultipartFile image,
+                                                       @RequestPart(value = "video", required = false) MultipartFile video) throws IOException {
+
+        this.userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("Post", "userId", userId));
+        Map<String, Object> updates = new ObjectMapper().readValue(updatesJson, new TypeReference<>() {});
+        if (image != null && !image.isEmpty()) {
+            String imageUrl = fileService.uploadImage(path, image);
+            updates.put("imageUrl", imageUrl);
+        }
+        if (video != null && !video.isEmpty()) {
+            String videoUrl = fileService.uploadVideo(path, video);
+            updates.put("videoUrl", videoUrl);
+        }
+        PostResponseBody updatePost = this.postService.patchPost(updates, postId);
+        return new ResponseEntity<>(updatePost, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/user/{userId}/posts/{postId}")
+    public ResponseEntity<APIResponse> deleteUser(@PathVariable Long userId, @PathVariable Long postId) {
+        this.userRepository.findById(userId).orElseThrow(() -> new ResourceNotFoundException("Post", "userId", userId));
         this.postService.deletePost(postId);
         return new ResponseEntity<>(new APIResponse("Post Deleted Successfully", true), HttpStatus.OK);
     }
@@ -69,7 +151,42 @@ public class PostController {
 
     @GetMapping("/search/{searchKey}")
     public ResponseEntity<List<PostResponseBody>> getPostsByTitleSearch(@PathVariable String searchKey) {
-        List<PostResponseBody> postDTOSBySearchKey = this.postService.searchPostsByTitle(searchKey);
-        return new ResponseEntity<>(postDTOSBySearchKey, HttpStatus.OK);
+        List<PostResponseBody> postResponseBodyList = this.postService.searchPostsByTitle(searchKey);
+        return new ResponseEntity<>(postResponseBodyList, HttpStatus.OK);
+    }
+
+//    @PostMapping("/image/upload/{postId}")
+//    public ResponseEntity<PostResponseBody> uploadPostImage(@RequestParam("image") MultipartFile image,
+//                                                         @PathVariable Long postId) throws IOException {
+//        PostResponseBody postResponseBody = this.postService.getPostById(postId);
+//
+    //        String imageFileName = this.fileService.uploadImage(path, image);
+//
+//        PostRequestBody postRequestBody = this.modelMapper.map(postResponseBody, PostRequestBody.class);
+//        postRequestBody.setImageUrl(imageFileName);
+//
+//        PostResponseBody updatedResponse = this.postService.updatePost(postRequestBody, postId);
+//        return new ResponseEntity<>(updatedResponse, HttpStatus.OK);
+//    }
+
+    @PostMapping("/image/upload")
+    public ResponseEntity<String> uploadPostImage(@RequestParam("image") MultipartFile image) throws IOException {
+        String imageFileName = this.fileService.uploadImage(path, image);
+        return new ResponseEntity<>(imageFileName, HttpStatus.OK); // Consider returning a full URL or a reference ID
+    }
+
+    @PostMapping("/video/upload")
+    public ResponseEntity<String> uploadPostVideo(@RequestParam("video") MultipartFile video) throws IOException {
+        String videoFileName = this.fileService.uploadVideo(path, video);
+        return new ResponseEntity<>(videoFileName, HttpStatus.OK); // Consider returning a full URL or a reference ID
+    }
+
+
+    @GetMapping(value = "/posts/image/{imageName}", produces = {MediaType.IMAGE_JPEG_VALUE,
+                                                MediaType.IMAGE_GIF_VALUE, MediaType.IMAGE_PNG_VALUE})
+    public void downloadImage(@PathVariable String imageName, HttpServletResponse httpServletResponse) throws IOException {
+        InputStream resource = this.fileService.getResource(path, imageName);
+        httpServletResponse.setContentType(MediaType.IMAGE_JPEG_VALUE);
+        StreamUtils.copy(resource, httpServletResponse.getOutputStream());
     }
 }
