736d82b146bb1892e8d46ea383e52abcd53d3ca5
Jitendra Tiwari <48156628+jkjitendra@users.noreply.github.com>
2024-04-03 23:54:20 +0530

    Feature/initial setup (#30)
    * Implemented Category CRUD and added request validation for Category and User with global exception handling
    
    * added Post, Tag, Profile Entity
    
    * Refactor User entity: Convert primitives to wrappers, add comments field
    
    * Refactor Category entity: Convert primitives to wrappers
    
    * Refactor: Convert primitives to wrappers
    
    * Added Post entity, service, repo, and CRUD in controller
    
    * added Comment Entity
    
    * Refactor User and Post DTOs and Enhance Validation
    
    * Add Pagination and Sorting to getAll method in PostService
    
    * updated springboot version, added google libphonenumber for validation, updated User crud operations, added checkusernameavailability feature, added mobile and countryName to USER
    
    * added @Transactional annotation,
    added ExceptionHandler for IllegalArgumentException,
    Updated ResourceNotFoundException to accept String as well as Long values
    
    * added constants,
    added configuration for file upload,
    implemented fileService for file upload,
    added CountryToRegionCodeUtil and PhoneNumberValidationUtil class
    
    * updated hibernate dialect config,
    added show-sql
    
    * added Profile CRUD operations
    
    * updated mobile number format to store as (+,countryCode, mobile)
    
    * separated phonenumber format creation.
    
    * updated Profile entity with @JsonStringType
    
    * updated requestbody name from updates to profile for patchProfile
    
    * Add countryCode to updateUser API for mobile number
    
    * Updated UniqueConstraint for Tag Entity, added findByTagName method to TagRepository
    
    * Extended PostResponseBody with postId and tagNames fields.
    Enhanced PostRequestBody with comments and tagNames fields."
    
    * Added searchKeyOnTitle method to the PostRepository.
    
    * PostController CRUD operations
    
    * added Longtext to postContent column, updated name of post_tag name, added new methods
    
    * commented out comments in postRequestBody
    
    * added visibility api, added createdDate, lastUpdatedDate and comments in PostResponseBody
    
    * updated createPost params and patchPost params, added toggleVisibility method
    
    * updated createPost, getPostById logic to include tagNames in response, updated getAllPost logic to include top 2 comments in response, updated updatePost to include tagNames, isLive, lastUpdatedDate in request and response, added patchPost method and togglePostVisibility

--- backend/blog/src/main/java/com/jk/blog/service/impl/PostServiceImpl.java
+++ backend/blog/src/main/java/com/jk/blog/service/impl/PostServiceImpl.java
@@ -1,15 +1,10 @@
 package com.jk.blog.service.impl;
 
-import com.jk.blog.dto.PageableResponse;
-import com.jk.blog.dto.PostRequestBody;
-import com.jk.blog.dto.PostResponseBody;
-import com.jk.blog.entity.Category;
-import com.jk.blog.entity.Post;
-import com.jk.blog.entity.User;
+import com.jk.blog.constants.AppConstants;
+import com.jk.blog.dto.*;
+import com.jk.blog.entity.*;
 import com.jk.blog.exception.ResourceNotFoundException;
-import com.jk.blog.repository.CategoryRepository;
-import com.jk.blog.repository.PostRepository;
-import com.jk.blog.repository.UserRepository;
+import com.jk.blog.repository.*;
 import com.jk.blog.service.PostService;
 import org.modelmapper.ModelMapper;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -18,8 +13,15 @@ import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Sort;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.util.ReflectionUtils;
+import org.modelmapper.TypeMap;
 
+import java.lang.reflect.Field;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
 import java.util.stream.Collectors;
 
 @Service
@@ -32,47 +34,102 @@ public class PostServiceImpl implements PostService {
     @Autowired
     private CategoryRepository categoryRepository;
     @Autowired
+    private TagRepository tagRepository;
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
     private ModelMapper modelMapper;
 
     @Override
-    public PostResponseBody createPost(PostRequestBody postRequestBody) {
+    @Transactional
+    public PostResponseBody createPost(Long userId, PostRequestBody postRequestBody) {
 
-        User user = userRepository
-                    .findById(postRequestBody.getUserId())
-                    .orElseThrow(() -> new ResourceNotFoundException("User", "userId", postRequestBody.getUserId()));
-        Category category = categoryRepository.findById(postRequestBody.getCategoryId()).orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", postRequestBody.getCategoryId()));
-        Post post = this.modelMapper.map(postRequestBody, Post.class);
+        User user = this.userRepository
+                        .findById(userId)
+                        .orElseThrow(() -> new ResourceNotFoundException("User", "userId", userId));
+        Category category = this.categoryRepository
+                                .findById(postRequestBody.getCategoryId())
+                                .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", postRequestBody.getCategoryId()));
 
+        Post post = new Post();
+        post.setPostTitle(postRequestBody.getTitle());
+        post.setPostContent(postRequestBody.getContent());
+        post.setImageUrl(postRequestBody.getImageUrl());
+        post.setVideoUrl(postRequestBody.getVideoUrl());
+        post.setLive(true);
         post.setUser(user);
         post.setCategory(category);
+
+        if (postRequestBody.getTagNames() != null && !postRequestBody.getTagNames().isEmpty()) {
+            Set<Tag> tags = postRequestBody.getTagNames().stream()
+                                           .map(tagName -> this.tagRepository.findByTagName(tagName)
+                                           .orElseGet(() -> this.tagRepository.save(new Tag(tagName))))
+                                           .collect(Collectors.toSet());
+            post.setTags(tags);
+        }
 /*
         Optional if using @PrePersist and @PreUpdate
         post.setCreatedDate(new Date());
         post.setLastUpdatedDate(new Date());
 */
         Post savedPost = this.postRepository.save(post);
-        return this.modelMapper.map(savedPost, PostResponseBody.class);
+        PostResponseBody postResponseBody = this.modelMapper.map(savedPost, PostResponseBody.class);
+
+        postResponseBody.setIsLive(savedPost.isLive());
+
+        Set<String> tagNames = savedPost.getTags().stream()
+                .map(Tag::getTagName)
+                .collect(Collectors.toSet());
+        postResponseBody.setTagNames(tagNames);
+
+        return postResponseBody;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public PostResponseBody getPostById(Long postId) {
         Post post = this.postRepository
                         .findById(postId)
                         .orElseThrow(() -> new ResourceNotFoundException("Post", "postId", postId));
-        return this.modelMapper.map(post, PostResponseBody.class);
+        PostResponseBody responseBody = this.modelMapper.map(post, PostResponseBody.class);
 
+
+        List<CommentResponseBody> commentResponses = post.getComments().stream()
+                                                         .map(comment -> this.modelMapper.map(comment, CommentResponseBody.class))
+                                                         .collect(Collectors.toList());
+        responseBody.setComments(commentResponses);
+        responseBody.setIsLive(post.isLive());
+        responseBody.setLastUpdatedDate(post.getLastUpdatedDate());
+        // Convert tag names to a set of strings
+        Set<String> tagNames = post.getTags().stream()
+                .map(Tag::getTagName)
+                .collect(Collectors.toSet());
+        responseBody.setTagNames(tagNames);
+
+        return responseBody;
     }
 
     @Override
+    @Transactional(readOnly = true)
     public PageableResponse<PostResponseBody> getAllPost(Integer pageNumber, Integer pageSize, String sortBy, String sortDirection) {
-        Sort sort = sortDirection.equalsIgnoreCase("asc") ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
+        Sort sort = sortDirection.equalsIgnoreCase(AppConstants.SORT_DIR) ? Sort.by(sortBy).ascending() : Sort.by(sortBy).descending();
         Pageable pageable = PageRequest.of(pageNumber, pageSize, sort);
         Page<Post> pagePost = this.postRepository.findAll(pageable);
         List<Post> postList = pagePost.getContent();
 
-        List<PostResponseBody> postResponseBodyList = postList.stream()
-                                                       .map((eachPost) -> this.modelMapper.map(eachPost, PostResponseBody.class))
-                                                       .collect(Collectors.toList());
+        List<PostResponseBody> postResponseBodyList = postList.stream().map(post -> {
+            PostResponseBody postResponse = modelMapper.map(post, PostResponseBody.class);
+
+            // Fetch a limited number of comments for each post, for example, the two most recent comments
+            List<Comment> comments = this.commentRepository.findTop2ByPost_PostIdOrderByCreatedDateDesc(post.getPostId());
+            List<CommentResponseBody> commentResponses = comments.stream()
+                    .map(comment -> modelMapper.map(comment, CommentResponseBody.class))
+                    .collect(Collectors.toList());
+            postResponse.setComments(commentResponses);
+            postResponse.setIsLive(post.isLive());
+            postResponse.setLastUpdatedDate(post.getLastUpdatedDate());
+            return postResponse;
+        }).collect(Collectors.toList());
 
         PageableResponse<PostResponseBody> pageableResponse = new PageableResponse<>();
         pageableResponse.setContent(postResponseBodyList);
@@ -85,28 +142,83 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
+    @Transactional
     public PostResponseBody updatePost(PostRequestBody postRequestBody, Long postId) {
+
         Post existingPost = this.postRepository
+                                .findById(postId)
+                                .orElseThrow(() -> new ResourceNotFoundException("Post", "postId", postId));
+
+        Category category = this.categoryRepository
+                                .findById(postRequestBody.getCategoryId())
+                                .orElseThrow(() -> new ResourceNotFoundException("Category", "categoryId", postRequestBody.getCategoryId()));
+
+        if (postRequestBody.getTagNames() != null && !postRequestBody.getTagNames().isEmpty()) {
+            // Fetch or create new tags and update the association
+            Set<Tag> updatedTags = postRequestBody.getTagNames().stream()
+                                                  .map(tagName -> tagRepository.findByTagName(tagName)
+                                                  .orElseGet(() -> tagRepository.save(new Tag(tagName))))
+                                                  .collect(Collectors.toSet());
+            // Set the updated tags to the post
+            existingPost.getTags().clear(); // Clear existing tags first
+            existingPost.getTags().addAll(updatedTags); // Add all the updated tags
+        }
+
+        existingPost.setPostTitle(postRequestBody.getTitle());
+        existingPost.setPostContent(postRequestBody.getContent());
+        if (postRequestBody.getImageUrl() != null) {
+            existingPost.setImageUrl(postRequestBody.getImageUrl());
+        }
+        if (postRequestBody.getVideoUrl() != null) {
+            existingPost.setVideoUrl(postRequestBody.getVideoUrl());
+        }
+//        existingPost.setLive(true);
+        existingPost.setCategory(category);
+        Post updatedPost = this.postRepository.save(existingPost);
+        PostResponseBody postResponseBody = this.modelMapper.map(updatedPost, PostResponseBody.class);
+
+        postResponseBody.setIsLive(updatedPost.isLive());
+        postResponseBody.setLastUpdatedDate(updatedPost.getLastUpdatedDate());
+
+        Set<String> tagNames = updatedPost.getTags().stream()
+                .map(Tag::getTagName)
+                .collect(Collectors.toSet());
+        postResponseBody.setTagNames(tagNames);
+
+        return postResponseBody;
+    }
+
+    @Override
+    @Transactional
+    public PostResponseBody patchPost(Map<String, Object> updates, Long postId) {
+        Post post = this.postRepository
                         .findById(postId)
                         .orElseThrow(() -> new ResourceNotFoundException("Post", "postId", postId));
-    /*
-         post.setPostTitle(postDTO.getTitle());
-         post.setPostContent(postDTO.getContent());
-         post.setImageUrl(postDTO.getImageUrl());
-         post.setVideoUrl(postDTO.getVideoUrl());
-         post.setLive(postDTO.getIsLive());
-    */
-        // above is not needed if used below 82 to 85
-        this.modelMapper.typeMap(PostRequestBody.class, Post.class).addMappings(mapper -> {
-            mapper.skip(Post::setUser);
-        });
-        this.modelMapper.map(postRequestBody, existingPost);
 
-        Post updatedPost = this.postRepository.save(existingPost);
+        updates.forEach((key, value) -> {
+            Field field = ReflectionUtils.findField(Post.class, key);
+            if (field != null) {
+                field.setAccessible(true);
+                ReflectionUtils.setField(field, post, value);
+            }
+        });
+
+        Post updatedPost = this.postRepository.save(post);
         return this.modelMapper.map(updatedPost, PostResponseBody.class);
     }
 
     @Override
+    @Transactional
+    public void togglePostVisibility(Long postId, boolean isLive) {
+        Post post = this.postRepository
+                        .findById(postId)
+                        .orElseThrow(() -> new ResourceNotFoundException("Post", "postId", postId));
+        post.setLive(isLive);
+        this.postRepository.save(post);
+    }
+
+    @Override
+    @Transactional
     public void deletePost(Long postId) {
         Post existingPost = this.postRepository
                                 .findById(postId)
@@ -115,6 +227,7 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<PostResponseBody> getPostsByUser(Long userId) {
         User existingUser = this.userRepository
                 .findById(userId)
@@ -126,6 +239,7 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<PostResponseBody> getPostsByCategory(Long categoryId) {
         Category existingCategory = this.categoryRepository
                                         .findById(categoryId)
@@ -137,8 +251,9 @@ public class PostServiceImpl implements PostService {
     }
 
     @Override
+    @Transactional(readOnly = true)
     public List<PostResponseBody> searchPostsByTitle(String keyword) {
-        List<Post> existingPostList = this.postRepository.findByTitleContainingIgnoreCase(keyword);
+        List<Post> existingPostList = this.postRepository.searchKeyOnTitle("%" + keyword + "%");
         return existingPostList.stream()
                 .map((eachPost) -> this.modelMapper.map(eachPost, PostResponseBody.class))
                 .collect(Collectors.toList());
