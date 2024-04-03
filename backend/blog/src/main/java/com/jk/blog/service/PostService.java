826c6f9039d1572d8108e0cb07a7b171e2770fc8
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-04-03 23:26:47 +0530

    updated createPost params and patchPost params, added toggleVisibility method

--- backend/blog/src/main/java/com/jk/blog/service/PostService.java
+++ backend/blog/src/main/java/com/jk/blog/service/PostService.java
@@ -3,12 +3,16 @@ package com.jk.blog.service;
 import com.jk.blog.dto.PageableResponse;
 import com.jk.blog.dto.PostRequestBody;
 import com.jk.blog.dto.PostResponseBody;
+import com.jk.blog.dto.ProfileResponseBody;
 
 import java.util.List;
+import java.util.Map;
 
 public interface PostService {
-    PostResponseBody createPost(PostRequestBody postRequestBody);
+    PostResponseBody createPost(Long userId, PostRequestBody postRequestBody);
     PostResponseBody updatePost(PostRequestBody postRequestBody, Long postId);
+    PostResponseBody patchPost(Map<String, Object> updates, Long postId);
+    void togglePostVisibility(Long postId, boolean isLive);
     void deletePost(Long postId);
     PageableResponse<PostResponseBody> getAllPost(Integer pageNumber, Integer pageSize, String sortBy, String sortDirection);
     PostResponseBody getPostById(Long postId);
