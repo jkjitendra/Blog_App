d3c437fd6014a1d6a6ddc06b44ed9a9876c7b691
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-04-05 00:16:57 +0530

    CRUD apis for commentController

---
+++ backend/blog/src/main/java/com/jk/blog/controller/CommentController.java
@@ -0,0 +1,52 @@
+package com.jk.blog.controller;
+
+import com.jk.blog.dto.*;
+import com.jk.blog.service.CommentService;
+import jakarta.validation.Valid;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.io.IOException;
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/comments")
+public class CommentController {
+
+    @Autowired
+    private CommentService commentService;
+
+    @PostMapping("/post/{postId}/comments")
+    public ResponseEntity<CommentResponseBody> createComment(@RequestBody CommentRequestBody commentRequestBody, @PathVariable Long postId) {
+        CommentResponseBody commentResponseBody = this.commentService.createComment(commentRequestBody, postId);
+        return new ResponseEntity<>(commentResponseBody, HttpStatus.CREATED);
+    }
+
+    @GetMapping("/post/{postId}")
+    public ResponseEntity<List<CommentResponseBody>> getAllComment(@PathVariable Long postId) {
+        List<CommentResponseBody> commentList = this.commentService.getAllComments(postId);
+        return new ResponseEntity<>(commentList, HttpStatus.OK);
+    }
+
+    @GetMapping("/comment/{commentId}")
+    public ResponseEntity<CommentResponseBody> getCommentById(@PathVariable Long commentId) {
+        CommentResponseBody commentResponseBody = this.commentService.getCommentById(commentId);
+        return new ResponseEntity<>(commentResponseBody, HttpStatus.OK);
+    }
+
+    @PutMapping("/comment/{commentId}")
+    public ResponseEntity<CommentResponseBody> updateComment(@Valid @RequestBody CommentRequestBody commentRequestBody,
+                                                       @PathVariable Long commentId) throws IOException {
+
+        CommentResponseBody updatedComment = this.commentService.updateComment(commentRequestBody, commentId);
+        return new ResponseEntity<>(updatedComment, HttpStatus.OK);
+    }
+
+    @DeleteMapping("/comment/{commentId}")
+    public ResponseEntity<APIResponse> deleteComment(@PathVariable Long commentId) {
+        this.commentService.deleteComment(commentId);
+        return new ResponseEntity<>(new APIResponse("Comment deleted Successfully", true), HttpStatus.OK);
+    }
+}
