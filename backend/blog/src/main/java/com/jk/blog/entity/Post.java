70ca53ac3a7a415d81a75f84f9e653b38ad7ab18
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-04-10 23:24:42 +0530

    added lastUpdatedDate, isPostDeleted, postDeletionTimestamp fields to Post and removed unwanted methods

--- backend/blog/src/main/java/com/jk/blog/entity/Post.java
+++ backend/blog/src/main/java/com/jk/blog/entity/Post.java
@@ -1,12 +1,12 @@
 package com.jk.blog.entity;
 
-import com.fasterxml.jackson.annotation.JsonIdentityInfo;
-import com.fasterxml.jackson.annotation.ObjectIdGenerators;
 import jakarta.persistence.*;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
+import lombok.ToString;
 
+import java.time.Instant;
 import java.util.*;
 
 @Entity
@@ -14,10 +14,7 @@ import java.util.*;
 @NoArgsConstructor
 @Getter
 @Setter
-//@JsonIdentityInfo(
-//        generator = ObjectIdGenerators.PropertyGenerator.class,
-//        property = "postId"
-//)
+@ToString
 public class Post {
 
     @Id
@@ -34,11 +31,13 @@ public class Post {
     private String imageUrl;
     private String videoUrl;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date createdDate;
+    private Instant createdDate;
 
-    @Temporal(TemporalType.TIMESTAMP)
-    private Date lastUpdatedDate;
+    private Instant lastUpdatedDate;
+
+    private boolean isPostDeleted = false;
+
+    private Instant postDeletionTimestamp;
 
     @Column(nullable = false)
     private boolean isLive = true; // Default value set to true
@@ -62,35 +61,4 @@ public class Post {
     )
     private Set<Tag> tags = new HashSet<>();
 
-    @PrePersist
-    protected void onCreate() {
-        createdDate = new Date();
-    }
-
-    @PreUpdate
-    protected void onUpdate() {
-        lastUpdatedDate = new Date();
-    }
-
-    // Utility methods for adding/removing comments
-    public void addComment(Comment comment) {
-        this.comments.add(comment);
-        comment.setPost(this);
-    }
-
-    public void removeComment(Comment comment) {
-        this.comments.remove(comment);
-        comment.setPost(null);
-    }
-
-    // Utility methods for adding/removing tags
-    public void addTag(Tag tag) {
-        this.tags.add(tag);
-        tag.getPosts().add(this);
-    }
-
-    public void removeTag(Tag tag) {
-        this.tags.remove(tag);
-        tag.getPosts().remove(this);
-    }
 }
