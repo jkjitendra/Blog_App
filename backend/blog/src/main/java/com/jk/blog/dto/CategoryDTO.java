9cde6505d48c31b2914477599e51f15a02e3f019
Jitendra Tiwari <48156628+jkjitendra@users.noreply.github.com>
2024-04-11 23:55:32 +0530

    Feature/initial setup (#37)
    * Implemented Category CRUD and added request validation for Category and User with global exception handling
    
    * added Post, Tag, Profile Entity
    
    * Refactor User entity: Convert primitives to wrappers, add comments field
    
    * Refactor Category entity: Convert primitives to wrappers
    
    * Refactor: Convert primitives to wrappers
    
    * Added Post entity, service, repo, and CRUD in controller
    
    * added Comment Entity
    
    * Refactor User and Post DTOs and Enhance Validation
    
    * Add Pagination and Sorting to getAll method in PostService
    
    * updated springboot version, added google libphonenumber for validation, updated User crud operations, added checkusernameavailability feature, added mobile and countryName to USER
    
    * added @Transactional annotation,
    added ExceptionHandler for IllegalArgumentException,
    Updated ResourceNotFoundException to accept String as well as Long values
    
    * added constants,
    added configuration for file upload,
    implemented fileService for file upload,
    added CountryToRegionCodeUtil and PhoneNumberValidationUtil class
    
    * updated hibernate dialect config,
    added show-sql
    
    * added Profile CRUD operations
    
    * updated mobile number format to store as (+,countryCode, mobile)
    
    * separated phonenumber format creation.
    
    * updated Profile entity with @JsonStringType
    
    * updated requestbody name from updates to profile for patchProfile
    
    * Add countryCode to updateUser API for mobile number
    
    * Updated UniqueConstraint for Tag Entity, added findByTagName method to TagRepository
    
    * Extended PostResponseBody with postId and tagNames fields.
    Enhanced PostRequestBody with comments and tagNames fields."
    
    * Added searchKeyOnTitle method to the PostRepository.
    
    * PostController CRUD operations
    
    * added Longtext to postContent column, updated name of post_tag name, added new methods
    
    * commented out comments in postRequestBody
    
    * added visibility api, added createdDate, lastUpdatedDate and comments in PostResponseBody
    
    * updated createPost params and patchPost params, added toggleVisibility method
    
    * updated createPost, getPostById logic to include tagNames in response, updated getAllPost logic to include top 2 comments in response, updated updatePost to include tagNames, isLive, lastUpdatedDate in request and response, added patchPost method and togglePostVisibility
    
    * CRUD apis for commentController
    
    * added Comment request and response body
    
    * updated PatchPost method to handle patch request
    
    * added createdDate and lastUpdatedDate fields to PostResponseBody
    
    * added createdDate and lastLoggedInDate to User
    
    * added lastUpdatedDate, isPostDeleted, postDeletionTimestamp fields to Post and removed unwanted methods
    
    * added createdDate, lastUpdatedDate to Comment

--- backend/blog/src/main/java/com/jk/blog/dto/CategoryDTO.java
+++ backend/blog/src/main/java/com/jk/blog/dto/CategoryDTO.java
@@ -12,9 +12,11 @@ import lombok.Setter;
 public class CategoryDTO {
 
     private  Long categoryId;
+
     @NotBlank
     @Size(min = 4, message = "Min Size of Category Title is 4")
     private  String categoryTitle;
+
     @NotBlank
     @Size(min = 10, message = "Min Size of Category Description is 10")
     private  String categoryDescription;
