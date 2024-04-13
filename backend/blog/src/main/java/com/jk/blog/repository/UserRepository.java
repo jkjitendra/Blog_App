2d04a3bc3680dfe7a3d6135a510a697da91ab23b
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-04-14 00:02:41 +0530

    Added new methods to repositories for post and user entities

--- backend/blog/src/main/java/com/jk/blog/repository/UserRepository.java
+++ backend/blog/src/main/java/com/jk/blog/repository/UserRepository.java
@@ -2,12 +2,21 @@ package com.jk.blog.repository;
 
 import com.jk.blog.entity.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
+import java.time.Instant;
+import java.util.List;
 import java.util.Optional;
 
 public interface UserRepository extends JpaRepository<User, Long> {
+
     Optional<User> findByEmail(String email);
+
 //    Optional<User> findByResetTokenAndEmail(String resetToken, String email);
+
     boolean existsByUserName(String userName);
+
+    @Query("SELECT u FROM User u WHERE u.isUserDeleted = true AND u.userDeletionTimestamp < :cutoff")
+    List<User> findDeactivatedUsersBefore(Instant cutoff);
 
 }
