a257331bf680aebc7148cbeaaca8eb4faab9aebd
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-08-12 23:37:53 +0530

    refactor: Add generic type support to APIResponse class and new constructors

--- backend/blog/src/main/java/com/jk/blog/dto/APIResponse.java
+++ backend/blog/src/main/java/com/jk/blog/dto/APIResponse.java
@@ -1,6 +1,5 @@
 package com.jk.blog.dto;
 
-
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -10,10 +9,21 @@ import lombok.Setter;
 @Setter
 @NoArgsConstructor
 @AllArgsConstructor
-public class APIResponse {
-
-    private String message;
-
+public class APIResponse<T> {
     private Boolean success;
+    private String message;
+    private T data;
+    private Object errorDetails;
 
+    public APIResponse(Boolean success, String message) {
+        this.success = success;
+        this.message = message;
+    }
+
+    public APIResponse(Boolean success, String message, T data) {
+        this.success = success;
+        this.message = message;
+        this.data = data;
+    }
 }
+
