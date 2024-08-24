2a29cad49fb55977b601ca8a791ef88832e52369
Jitendra Tiwari <jkjitendra725@gmail.com>
2024-08-24 23:43:41 +0530

    Added RoleRepository to manage Role entity operations

---
+++ backend/blog/src/main/java/com/jk/blog/repository/RoleRepository.java
@@ -0,0 +1,9 @@
+package com.jk.blog.repository;
+
+import com.jk.blog.entity.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface RoleRepository extends JpaRepository<Role, Long> {
+    Role findByRoleName(String roleName);
+}
+
