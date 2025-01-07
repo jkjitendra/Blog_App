38b795bdfa9625679a7fc85ac39bd2a38159f918
Jitendra Tiwari <jkjitendra725@gmail.com>
2025-01-07 23:40:30 +0530

    - Refactor RoleRepository: Updated method signature and return type. - Improve SecurityConfig: Added session management and user repo dependency.

--- backend/blog/src/main/java/com/jk/blog/dto/RoleDTO.java
+++
@@ -1,6 +0,0 @@
-package com.jk.blog.dto;
-
-public class RoleDTO {
-
-    private String roleName;
-}
--- backend/blog/src/main/java/com/jk/blog/repository/RoleRepository.java
+++ backend/blog/src/main/java/com/jk/blog/repository/RoleRepository.java
@@ -1,9 +1,12 @@
 package com.jk.blog.repository;
 
 import com.jk.blog.entity.Role;
+import com.jk.blog.entity.RoleType;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.Optional;
+
 public interface RoleRepository extends JpaRepository<Role, Long> {
-    Role findByRoleName(String roleName);
+    Optional<Role> findByName(String roleName);
 }
 
