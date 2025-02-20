841e978e7114c4063bae6ef9a81da0cdba6ae013
Jitendra Tiwari <jkjitendra725@gmail.com>
2025-02-20 23:16:20 +0530

    feat(dto): Add default constructor for ResetPasswordDTO

--- backend/blog/src/main/java/com/jk/blog/dto/AuthDTO/ResetPasswordDTO.java
+++ backend/blog/src/main/java/com/jk/blog/dto/AuthDTO/ResetPasswordDTO.java
@@ -4,11 +4,13 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Pattern;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @AllArgsConstructor
+@NoArgsConstructor
 public class ResetPasswordDTO {
 
     @NotBlank(message = "Email cannot be blank")
