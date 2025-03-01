ac02537bfc33e7494eedf80d04141c6cc1268647
Jitendra Tiwari <48156628+jkjitendra@users.noreply.github.com>
2025-03-01 23:56:44 +0530

    Feature/initial setup (#198)

--- backend/blog/src/main/java/com/jk/blog/BlogApplication.java
+++ backend/blog/src/main/java/com/jk/blog/BlogApplication.java
@@ -2,12 +2,14 @@ package com.jk.blog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 import org.springframework.scheduling.annotation.EnableScheduling;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableMongoRepositories(basePackages = "com.jk.blog.repository.mongo")
+@EnableJpaRepositories(basePackages = "com.jk.blog.repository")
 public class BlogApplication {
 
 	public static void main(String[] args) {
