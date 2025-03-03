3f7dad083bc412481648fb92cb453ac80383a5ac
Jitendra Tiwari <48156628+jkjitendra@users.noreply.github.com>
2025-03-03 21:28:06 +0530

    Feature/initial setup (#202)

--- backend/blog/src/test/java/com/jk/blog/BlogApplicationTests.java
+++ backend/blog/src/test/java/com/jk/blog/BlogApplicationTests.java
@@ -1,10 +1,9 @@
 package com.jk.blog;
 
-import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@Disabled
+
 @SpringBootTest
 class BlogApplicationTests {
 
