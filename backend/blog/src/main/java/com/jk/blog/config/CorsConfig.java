1c80fe0814c1f3f6c5097a22ae9e574028002592
Jitendra Tiwari <48156628+jkjitendra@users.noreply.github.com>
2025-03-12 21:58:27 +0530

    Feature/oauth authentication (#229)

--- backend/blog/src/main/java/com/jk/blog/config/CorsConfig.java
+++ backend/blog/src/main/java/com/jk/blog/config/CorsConfig.java
@@ -6,6 +6,8 @@ import org.springframework.web.cors.CorsConfiguration;
 import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 import org.springframework.web.filter.CorsFilter;
 
+import java.util.List;
+
 @Configuration
 public class CorsConfig {
 
@@ -14,10 +16,11 @@ public class CorsConfig {
         UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
         CorsConfiguration config = new CorsConfiguration();
         config.setAllowCredentials(true);
-        config.addAllowedOrigin("http://localhost:5173");
+//        config.addAllowedOrigin("http://127.0.0.1:5500");
+        config.setAllowedOrigins(List.of("http://localhost:5500", "http://127.0.0.1:5500"));
         config.addAllowedOrigin("http://localhost:8080/swagger-ui/index.html");
         config.addAllowedHeader("*");
-        config.addAllowedMethod("*");
+        config.setAllowedMethods(List.of("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
         source.registerCorsConfiguration("/**", config);
         return new CorsFilter(source);
     }
